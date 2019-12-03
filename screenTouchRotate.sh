9d4052626442e7a02f2ae2b441bf2f2e35ddce2c
alphaOri <david.everett.smith@gmail.com>
2019-12-03 18:56:37 +0000

    screen/touch rotate script

---
+++ screenTouchRotate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this script rotates the display and touch input right 90 degrees
+
+xrandr -o right
+xinput set-prop 'TSTP MTouch' 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
+
